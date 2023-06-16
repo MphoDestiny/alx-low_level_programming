b283962ccb49519f84299100ef22995dca36d297
MphoDestiny <mphomasegomo@gmail.com>
2023-06-15 17:50:45 -0700

    5-printf.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
-#include <unistd.h>
-#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, n, strlen(n));
-return (1);
+    return (0);
 }
