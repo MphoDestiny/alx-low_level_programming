ed83b8e8c4aafccb1612a721eca2f54d8a1713f5
MphoDestiny <mphomasegomo@gmail.com>
2023-06-17 13:02:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower case
+ * Return: Always 0
+ */
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putcher(lc);
+	}
+	putcher('n\');
+	return (0);
+}
