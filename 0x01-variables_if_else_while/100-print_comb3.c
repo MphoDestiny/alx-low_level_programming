80512066b068415ea58f4e20a3ae6cc27a36f42f
MphoDestiny <mphomasegomo@gmail.com>
2023-06-20 03:49:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,32 +5,34 @@
  *
  * Return: always 0 (success)
  */
-int i = '0';
-{
-        int j = '1';
 
-        while (i <= '8')
-        {
-                while (j <= '9')
-                {
-                        if (!(i > j) || i == j)
-                        {
-                                putchar(i);
-                                putchar(j);
-                                if (i == '8' && j == '9')
-                                {
-                                        putchar('\n');
-                                }
-                                else
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                        j++;
-                }
-                j = '1';
-                i++;
-        }
-        return (0);
+int main(void)
+{
+	int i = '0';
+	int j = '1';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j) || i == j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	return (0);
 }
