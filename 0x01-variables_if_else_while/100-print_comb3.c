a6eb33ede9e77a898eb3fbf66fdbee7b84572ac1
MphoDestiny <mphomasegomo@gmail.com>
2023-06-20 08:28:23 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,18 @@ int main(void)
 
 	for (d = '0' ; d < '9' ; d++)
 	{
-		for (p = d + 1 ; p <= '9')
-
+		for (p = d + 1 ; p <= '9' ; p++)
+		{
 			if (p != d)
 			{
-				putchar (p);
 				putchar (d);
+				putchar (p);
 				if (d == '8' && p == '9')
 					continue;
 				putchar (',');
-				putchar(' ');
+				putchar (' ');
 			}
+		}
 	}
 	putchar ('\n');
 	return (0);
