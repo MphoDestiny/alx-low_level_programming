1394ff1c46445dc0bc854c2cbed01f6fc61b8faf
MphoDestiny <mphomasegomo@gmail.com>
2023-06-20 03:52:18 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 
 	while (i <= '9')
 	{
-		while (j <= '9')
+		while (j <= '10')
 		{
 			if (!(i > j) || i == j)
 			{
 				putchar(i);
 				putchar(j);
-				if (i == '8' && j == '9')
+				if (i == '9' && j == '10')
 				{
 					putchar('\n');
 				}
