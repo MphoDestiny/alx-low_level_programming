c74a174c0f931da597168c561a3dfd17fafa8833
MphoDestiny <mphomasegomo@gmail.com>
2023-06-23 01:42:07 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,30 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_triangle - function that prints a triangle
- * @size: size of the triangle
- * Return: triangle of '#'s
- */
-void print_triangle(int size)
-{
-int sp, ro, tr;
-if (Size <= 0)
-{
-	_putchar('\n');
-}
-
-else
-{
-	for (ro = 0; ro <= (size - 1); ro++)
-	{
-		for (sp = 0; sp < (size - 1) - ro; sp++)
-
-		{
-		_putchar(' ');
-		}
-	for (tr = 0; tr <= ro; tr++)
-	{
-	_putchar('#');
-	}
-_putchar('\n');
-	}
-}
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints 
