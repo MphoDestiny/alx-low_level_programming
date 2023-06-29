398eb14a5c52db082d1338b0a5315c53ff4e34f4
MphoDestiny <mphomasegomo@gmail.com>
2023-06-29 03:16:08 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -43,8 +43,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (d == 1)
 	{
 		r[l + 1] = '\0';
-		if (l + 2 > size_r)
-				return (0);
+		if (l + 2 > isize_r)
+			return (0);
 		while (l-- >= 0)
 			r[l + 1] = r[l];
 		r[0] = d + '\0';
