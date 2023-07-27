e23191d0c7d00497794cd58bedea82c0f0133ba5
MphoDestiny <mphomasegomo@gmail.com>
2023-07-27 07:30:02 -0700

    lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,14 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the lements of a linked list
- * @h: linked list of type listint_t to print
+ * print_list - prints all the lements of a linked list
+ * @h: linked list of type list_t to print
  *
  * Return: number of nodes.
  */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
